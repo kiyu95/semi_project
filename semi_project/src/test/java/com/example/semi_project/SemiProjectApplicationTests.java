4084cde6ecae27d670095a46624f1391ec5d95c6
kiyu <giu0428@gmail.com>
2023-12-14 18:22:01 +0900

    list

---
+++ semi_project/src/test/java/com/example/semi_project/SemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
